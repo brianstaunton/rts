43d63d070c9647e0175555ce59e86817e4f5c987
Brian Staunton <brian.staunton@gmail.com>
2023-06-19 09:11:58 +0000

    First Project Completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+import UserSearch from './refs/UserSearch';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return (
@@ -9,4 +11,11 @@ const App = () => {
     </div>
   );
 };
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = createRoot(rootElement);
+root.render(
+<StrictMode><App /></StrictMode>);
+
--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const users = [
     { name: 'Sarah', age: 20 },
@@ -6,9 +6,16 @@ const users = [
     { name: 'Alex', age: 20 }
 ]
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
-
   const [user, setUser] = useState<{ name: string, age: number} | undefined>();
+ 
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find((user) => {
@@ -19,7 +26,10 @@ const UserSearch: React.FC = () => {
 
   return <div>
     User Search 
-    <input value={(name)} onChange={ e => setName(e.target.value)} />
+    <input 
+      ref={inputRef} 
+      value={(name)} 
+      onChange={ e => setName(e.target.value)} />
     <button onClick={onClick}>Find User</button>
     <div>
         { user && user.name }
